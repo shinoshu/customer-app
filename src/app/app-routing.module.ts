170260327393548c627949b0fb566ee86ca805f9
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-11-29 18:55:36 +0900

    ng generate module customers --route customer-list --module app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'customer-list', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
